fe743bbfed03527c79e74ff109e5ce3835d74f0d
Noam Raz <noamraz8@gmail.com>
2016-12-15 21:41:16 +0200

    Implement basic process enumeration logic

--- pyps.py
+++ pyps.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python
+
+import os
+
+def is_pid(name):
+    '''Returns whether name is a valid pid'''
+    try:
+        int(name)  # pids are just integers
+        return True
+    except ValueError:
+        return False
+
+def enum_procs(cb):
+    '''enumerates all running processes and calls cb with the pid of each one'''
+    for name in filter(is_pid, os.listdir('/proc')):
+        cb(name)
+
+
+# quick test:
+def print_pid(pid):
+    print pid
+enum_procs(print_pid)
